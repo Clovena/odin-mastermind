fdf52766cb8cfa08bd657d93a77aed76f700075b
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-04-17 23:44:51 -0400

    Finish programming game. WIP: test for bugs in gets.

--- lib/game.rb
+++ lib/game.rb
@@ -8,19 +8,28 @@ class Game
     @code = Codemaker.new_code
   end
 
-  def make_guess
-    puts 'Take a guess: '
-    guess = gets.chomp
+  def cracked(input, solution)
+    input.split('') == solution
+  end
 
-    total_pegs = CorrectPegs.all_pegs(guess, code)
-    full_correct = CorrectPegs.proper_place(guess, code)
+  def get_pegs(input, solution)
+    total_pegs = CorrectPegs.all_pegs(input, solution)
+    full_correct = CorrectPegs.proper_place(input, solution)
     partial_correct = total_pegs - full_correct
 
     puts "Correct numbers with correct placement: #{full_correct}"
     puts "Correct numbers with wrong placement: #{partial_correct}."
   end
 
-  def self.cracked(input, solution)
-    input == solution
+  def make_guess
+    puts 'Take a guess: '
+    guess = gets.chomp
+
+    if cracked(guess, code)
+      puts "Congratulations! You've cracked the code."
+      true # Break out of turns
+    else
+      get_pegs(guess, code)
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -2,49 +2,29 @@
 
 require_relative 'lib/codemaker'
 require_relative 'lib/pegs'
-
-# Store game metadata information
-class Game
-  attr_accessor :turn, :code
-
-  def initialize
-    @code = Codemaker.new_code
-  end
-
-  def make_guess
-    puts 'Take a guess: '
-    guess = gets.chomp
-
-    total_pegs = CorrectPegs.all_pegs(guess, code)
-    full_correct = CorrectPegs.proper_place(guess, code)
-    partial_correct = total_pegs - full_correct
-
-    puts "Correct numbers with correct placement: #{full_correct}"
-    puts "Correct numbers with wrong placement: #{partial_correct}."
-  end
-
-  def self.cracked(input, solution)
-    input == solution
-  end
-end
+require_relative 'lib/game'
 
 # Collect all information for one game
 class Mastermind
   attr_accessor :game
 
   def initialize
-    @turn = 0
     @game = Game.new
+    @turn = 0
+    @solved = false
   end
 
   def gameplay
-    while @turn < 5
-      @game.make_guess
+    puts 'Welcome to Mastermind!'
+    puts 'Generating a code...'
+    while @turn < 10 && !@solved
+      @solved = @game.make_guess
       @turn += 1
     end
+    puts 'Better luck next time...' if @turn == 10
   end
 end
 
 game1 = Mastermind.new
-p game1.game.code
+# p game1.game.code
 game1.gameplay
